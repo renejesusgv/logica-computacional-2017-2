66c2fa1ddc6a1bac38139fe9cf95873fb09a15cf
aquirozj <anahiqj@ciencias.unam.mx>
2017-04-08 02:11:17 -0500

    Agrego mintree y maxtree

--- practica5/arbol.pl
+++ practica5/arbol.pl
@@ -3,3 +3,13 @@ bt(node(A,T1,T2)):- integer(A),bt(T1),bt(T2).
 
 elem(A,bt(node(X,_T1,_T2))):- A==X,!.
 elem(A,bt(node(_X,T1,T2))):- elem(A,T1);elem(A,T2),!.
+
+maxtree(A,bt(node(X,T1,T2))):- A>X, maxtree(A,T1),maxtree(A,T2).
+maxtree(A,bt(node(X,bt(void),T2))):- A>X, maxtree(A,T2),!.
+maxtree(A,bt(node(X,T1,bt(void)))):- A>X, maxtree(A,T1),!.
+maxtree(A,bt(node(X,bt(void),bt(void)))):- A>X,!.
+
+mintree(A,bt(node(X,T1,T2))):- A<X, mintree(A,T1),mintree(A,T2).
+mintree(A,bt(node(X,bt(void),T2))):- A<X, mintree(A,T2),!.
+mintree(A,bt(node(X,T1,bt(void)))):- A<X, mintree(A,T1),!.
+mintree(A,bt(node(X,bt(void),bt(void)))):- A<X,!.
