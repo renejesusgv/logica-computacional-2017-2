64789cb73d2f644794a3e79bf00ab8ac9ebc5804
Felix Hernandez <felix.hn@ciencias.unam.mx>
2017-04-27 00:52:41 -0500

    Actualización

--- práctica6/practica6.pl
+++ práctica6/practica6.pl
@@ -1,16 +1,32 @@
-longitud([],0).
-longitud([_|H], N) :- longitud(H, N1), N is N1+1.
-
-
+/*Ejercicio 02*/
+/*	a)	*/
 ultimo([X|[]],X). 
 ultimo([_|H],X):- ultimo(H,X).
 
-/**recorrei(X,X,0).
-recorrei([X|Y], L, N):-N1 is N-1, append(Y,[X],Y1), recorrei(Y1, L, N1).
-*/
+/*	b)	*/
+longitud([],0).
+longitud([_|H], N) :- longitud(H, N1), N is N1+1.
 
+/*	c)	*/
 recorrei([H|T],Z) :- une(T, [H],Z).
 recorred(L, Z) :- recorrei(Z, L).
 
 une([],X,X).                            
 une([X|Y],Z,[X|W]) :- une(Y,Z,W). 
+
+/*Ejercicio 03*/
+
+/*	a)	*/
+transicion(q1,b,q2).
+transicion(q2,a,q2).
+transicion(q2,a,q3).
+transicion(q3,b,q2).
+inicial(q1).
+final(q3).
+
+/*	b)	*/
+acepta([], q3).
+acepta([H|T], estado) :- transicion(estado, H, X), acepta(T,X), final(X).
+
+
+
