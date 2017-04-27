6bdcc61e00b9294c88a94d114a4080fe4548998e
Felix Hernandez <felix.hn@ciencias.unam.mx>
2017-04-27 00:10:57 -0500

    agrego recorred

--- práctica6/practica6.pl
+++ práctica6/practica6.pl
@@ -10,6 +10,7 @@ recorrei([X|Y], L, N):-N1 is N-1, append(Y,[X],Y1), recorrei(Y1, L, N1).
 */
 
 recorrei([H|T],Z) :- une(T, [H],Z).
+recorred(L, Z) :- recorrei(Z, L).
 
 une([],X,X).                            
 une([X|Y],Z,[X|W]) :- une(Y,Z,W). 
