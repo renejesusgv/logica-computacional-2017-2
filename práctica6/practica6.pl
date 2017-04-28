3aed0fc7cfb319b377a5165130e6e2596198bfc4
aquirozj <anahiqj@ciencias.unam.mx>
2017-04-27 21:40:46 -0500

    Agrego autómata

--- práctica6/practica6.pl
+++ práctica6/practica6.pl
@@ -1,6 +1,19 @@
 longitud([],0).
 longitud([_|H], N) :- longitud(H, N1), N is N1+1.
 
-
 ultimo([X|[]],X). 
 ultimo([_|H],X):- ultimo(H,X).
+
+transicion(estado(1),b,estado(2)).
+transicion(estado(2),a,final(3)).
+transicion(estado(2),a,estado(2)).
+transicion(final(3),b,estado(2)).
+final(3).
+
+aceptaAux(final(3),[],final(3)).
+aceptaAux(E,[X|H],_):- transicion(E,X,Y), aceptaAux(Y,H,_).
+
+acepta(Y):- aceptaAux(estado(1),Y,_), final(3).
+
+
+
